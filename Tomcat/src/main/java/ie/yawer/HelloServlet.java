e60cd4e57400d03538423467fbeb5598a397b1b9
piotr.bar <piotr.bar@accenture.com>
2025-09-10 14:51:47 +0100

    Initialize project

---
+++ Tomcat/src/main/java/ie/yawer/HelloServlet.java
@@ -0,0 +1,31 @@
+package ie.yawer;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    // Display in a console to confirm the servlet was instantiated
+    public HelloServlet() {
+        System.out.println("HelloServlet initialized");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        // When origin (website) is hosted locally it can be for instance "http://localhost:8080"
+        String TRUSTED_REQUEST_ORIGIN = "*";
+        // Asterisk ("*") tells the browser: “Yes, it’s okay to accept requests from any origin.”
+        resp.setHeader("Access-Control-Allow-Origin", TRUSTED_REQUEST_ORIGIN);
+
+        // Display in a console to confirm the server was hit by request
+        System.out.println("Received GET request");
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+        // Response body's content
+        resp.getWriter().write("Hello World from tomcat!");
+    }
+}
